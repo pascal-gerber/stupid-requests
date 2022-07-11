4e9b261a929fa957195b3771b6b13395e044378a
Noodle doodle <77777914+pascal-gerber@users.noreply.github.com>
2022-07-11 23:28:29 +0200

    more additions
    more stuff

---
+++ Comments.py
@@ -0,0 +1,27 @@
+#the print function is here to print out a text, an integer, a float,
+#a list, a tuple, a dictionnary, a classobject, a type, a variable value,
+#a tkinter object, a module name, ect:
+#print is used often at beginning of programming at a "hello world" programm.
+#print can also be used to print out all the datatypes possible and deebugging.
+#debugging meaning testing of a programm while checking if the value is an expected
+#one or not, print is a function itself from its syntax on
+#since it has both brackets after, meaning the print function
+#is defined somewhere in the main python executable compiler, in fact
+#every function is always written this way "isnumeric()" which some
+#are predefined, print in particular has a different color in comparason
+#to a normal function since it counts as a main function
+#print is made in a deep python language and can be found somewhere where all the
+#other modules are hidden at.
+#print can take multiple arguments since it will print them out all anyways.
+#print also contains a auto calculation process meaning printing:
+#10 + 4 prints directly 14.
+#now imagine your own print function, since adding this command will reduce
+#trommendiously the amount of comments i have
+#you can imagine it instead riiiiiight here
+#||
+#\/
+
+
+
+
+#this lesson was sponsored by "Talentschool"
---
+++ copy pasted code.py
@@ -0,0 +1,12 @@
+a = ['a', 'b', 'mpilgrim', 'z', 'example']
+
+for i in <a>
+    <loop body>
+    print(i)
+
+
+#struggles
+    #deep struggles
+        #dont understant
+
+#thanks talentschool
